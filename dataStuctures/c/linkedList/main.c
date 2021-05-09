2fbb506bf5f144a9a655c64ae1eb43a094a9c0c1
Vladimir Efimov <vladimirefimov1@gmail.com>
2021-05-08 22:04:05 -0400

    Fixed spaces to tabs.

--- dataStuctures/c/linkedList/main.c
+++ dataStuctures/c/linkedList/main.c
@@ -10,35 +10,37 @@
 
 /* Print -> for debugging/testing, print out the list of int */
 void printIntList(struct linkedList* list) {
-        if (!list) {
-                return;
-        }
-        struct node *head = list->head;
-        // While head is not null
-        // Note: assuming value is int. Expect this to break hard otherwise.
-        while(head) {
-                printf("%d ", *((int*)(head->value)));
-                head = head->next;
-        }
+	if (!list) {
+		return;
+	}
+	struct node *head = list->head;
+	// While head is not null print value
+
+	while(head) {
+			// Note: assuming value is int.
+	// Expect this to break hard otherwise.
+		printf("%d ", *((int*)(head->value)));
+		head = head->next;
+	}
 	printf("\n");
 }
 
 int main(int argc, char **argv)
 {
-        struct linkedList* lst;
-        lst = init();
-        int a = 5;
-        int b = 1;
-        int c = 9;
-        int d = 88;
-        push(lst, &a);
-        push(lst, &b);
-        push(lst, &c);
-        append(lst, &d);
-        printIntList(lst);
-        printf("***\n");
-        printIntList(lst);
-        delete(lst);
-        return 0;
+	struct linkedList* lst;
+	lst = init();
+	int a = 5;
+	int b = 1;
+	int c = 9;
+	int d = 88;
+	push(lst, &a);
+	push(lst, &b);
+	push(lst, &c);
+	append(lst, &d);
+	printIntList(lst);
+	printf("***\n");
+	printIntList(lst);
+	delete(lst);
+	return 0;
 }
 
